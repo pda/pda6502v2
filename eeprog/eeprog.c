853bdd763ea79ed9d784222bd27d42aaef1af9cf
Paul Annesley <paul@annesley.cc>
2021-09-08 02:27:04 +1000

    eeprog: write & read "hello" to EEPROM over USB/FTDI/SPI bitbang.

--- eeprog/eeprog.c
+++ eeprog/eeprog.c
@@ -26,9 +26,28 @@
 #define EEPROM_RDSR  0x05 // Read Status Register
 #define EEPROM_WREN  0x06 // Enable Write Operations
 
-int spi_transfer(struct ftdi_context *ftdi, uint8_t * data);
+#define SPI_SELECT 0
+#define SPI_DESELECT 1
+
+#define CHECK(x) if (x != 0) return -1
+
+struct spi_context {
+  struct ftdi_context *ftdi;
+  uint8_t pins;
+  uint8_t data;
+};
+
+int spi_select(struct spi_context *spi);
+int spi_deselect(struct spi_context *spi);
+int spi_transfer(struct spi_context *, uint8_t mosi);
+int spi_read_pins(struct spi_context *);
+int spi_write_pins(struct spi_context *, uint8_t pins);
 void print_binary(uint8_t x);
 void print_pins(uint8_t p, char *msg);
+void print_status(uint8_t status);
+int test_things(struct spi_context *);
+
+int debug = 0;
 
 int main(int argc, char *argv[])
 {
@@ -65,34 +84,21 @@ int main(int argc, char *argv[])
 
   /* Enable bitbang */
   uint8_t mask = 1<<BIT_CS | 1<<BIT_SCK | 1<<BIT_MO | 0<<BIT_MI;
-  print_pins(mask, "IO mask; 0:input, 1:output");
   if (ftdi_set_bitmode(&ftdi, mask, BITMODE_BITBANG) < 0) {
     fprintf(stderr, "can't enable bitbang mode\n");
     exit(EXIT_FAILURE);
   }
   //printf("bitbang_enabled: 0x%02X, bitbang_mode: 0x%02X\n", ftdi.bitbang_enabled, ftdi.bitbang_mode);
 
-  uint8_t pins = 1<<BIT_CS | 0<<BIT_SCK | 0<<BIT_MO | 0<<BIT_MI;
-  print_pins(pins, "initial pin setup");
-  if ((ret = ftdi_write_data(&ftdi, &pins, 1)) < 0) {
-    fprintf(stderr, "ftdi_write_data failed: %d\n", ret);
-    return -1;
-  }
+  struct spi_context spi = {
+    .ftdi = &ftdi,
+    .pins = 1<<BIT_CS | 0<<BIT_SCK | 0<<BIT_MO | 0<<BIT_MI,
+  };
+  spi_write_pins(&spi, spi.pins);
 
-  uint8_t data = EEPROM_RDSR;
-  pins &= ~(1<<BIT_CS);
-  if ((ret = ftdi_write_data(&ftdi, &pins, 1)) < 0) {
-    fprintf(stderr, "ftdi_write_data failed: %d\n", ret);
-    return -1;
+  if (test_things(&spi) != 0) {
+    fprintf(stderr, "test_things failed, hopefully it produced some stderr\n");
   }
-  spi_transfer(&ftdi, &data); // write command
-  spi_transfer(&ftdi, &data); // read status register
-  pins |= 1<<BIT_CS;
-  if ((ret = ftdi_write_data(&ftdi, &pins, 1)) < 0) {
-    fprintf(stderr, "ftdi_write_data failed: %d\n", ret);
-    return -1;
-  }
-  printf("status: 0x%02X\n", data);
 
   if ((ret = ftdi_usb_close(&ftdi)) < 0) {
     fprintf(stderr, "unable to close ftdi device: %d (%s)\n", ret, ftdi_get_error_string(&ftdi));
@@ -105,52 +111,141 @@ int main(int argc, char *argv[])
   return EXIT_SUCCESS;
 }
 
-int spi_transfer(struct ftdi_context *ftdi, uint8_t * data) {
-  uint8_t pins;
-  int ret;
-  // get current state of pins
-  if (ftdi_read_pins(ftdi, &pins) < 0) {
-    fprintf(stderr, "ftdi_read_pins failed\n");
-    return -1;
+int test_things(struct spi_context *spi) {
+  uint32_t addr;
+
+  printf("-- enable writes\n");
+  CHECK(spi_select(spi));
+  CHECK(spi_transfer(spi, EEPROM_WREN));
+  CHECK(spi_deselect(spi));
+
+  printf("-- read status register\n");
+  CHECK(spi_select(spi));
+  CHECK(spi_transfer(spi, EEPROM_RDSR));
+  CHECK(spi_transfer(spi, 0));
+  print_status(spi->data);
+  CHECK(spi_deselect(spi));
+
+  //addr = 1034;
+  //printf("-- write data\n");
+  //CHECK(spi_select(spi));
+  //CHECK(spi_transfer(spi, EEPROM_WRITE));
+  //CHECK(spi_transfer(spi, addr>>16&0xFF)); // ADDR[23:16]
+  //CHECK(spi_transfer(spi, addr>>8&0xFF)); // ADDR[15:8]
+  //CHECK(spi_transfer(spi, addr&0xFF)); // ADDR[7:0]
+  //CHECK(spi_transfer(spi, 'h'));
+  //CHECK(spi_transfer(spi, 'e'));
+  //CHECK(spi_transfer(spi, 'l'));
+  //CHECK(spi_transfer(spi, 'l'));
+  //CHECK(spi_transfer(spi, 'o'));
+  //CHECK(spi_deselect(spi));
+
+  addr = 0x000380;
+  printf("-- read data\n");
+  CHECK(spi_select(spi));
+  CHECK(spi_transfer(spi, EEPROM_READ));
+  CHECK(spi_transfer(spi, addr>>16&0xFF)); // ADDR[23:16]
+  CHECK(spi_transfer(spi, addr>>8&0xFF)); // ADDR[15:8]
+  CHECK(spi_transfer(spi, addr&0xFF)); // ADDR[7:0]
+  // sneaky 2am hexdump
+  char asciibuf[16];
+  for (int i = 0; i < 256; i++) {
+    CHECK(spi_transfer(spi, 0));
+    asciibuf[i%16] = (spi->data >= 32 && spi->data <= 126) ? spi->data : '.';
+    if (i % 16 == 0) printf("%08x  ", addr+i);
+    printf("%02x", spi->data);
+    if (i % 16 == 7) printf(" ");
+    if (i % 16 == 15) {
+      printf("  |");
+      for (int j = 0; j < sizeof(asciibuf); j++) {
+        putc(asciibuf[j], stdout);
+      }
+      printf("|\n");
+    } else {
+      printf(" ");
+    }
   }
-  print_pins(pins, "spi_transfer start");
-  if ((pins & 1<<BIT_SCK) != 0) {
-    fprintf(stderr, "spi_transfer expects low clock\n");
-    return -1;
+  printf("\n");
+  CHECK(spi_deselect(spi));
+
+  return 0;
+}
+
+// int spi_init(struct spi_context *spi, ...) {
+//   ...
+//   // get current state of pins
+//   if (ftdi_read_pins(ftdi, &pins) < 0) {
+//     fprintf(stderr, "ftdi_read_pins failed\n");
+//     return -1;
+//   }
+//   ...
+// }
+
+int spi_select(struct spi_context *spi) {
+  return spi_write_pins(spi, spi->pins & ~(1<<BIT_CS));
+}
+
+int spi_deselect(struct spi_context *spi) {
+  return spi_write_pins(spi, spi->pins | 1<<BIT_CS);
+}
+
+// note: on error, spi->pins may be left out of sync with reality.
+int spi_transfer(struct spi_context *spi, uint8_t data) {
+  if (debug) {
+    printf("MOSI → 0x%02X ", data);
+    print_binary(data);
+    printf("\n");
   }
   for (int i = 0; i <= 7; i++) {
-    printf("MOSI: %d\n", *data>>7);
-    // during low clock, set MSB from data buffer into MOSI bit of pins buffer
-    pins = (pins & ~(1<<BIT_MO)) | ((*data>>7) << BIT_MO);
-    print_pins(pins, "set MOSI");
-    if ((ret = ftdi_write_data(ftdi, &pins, 1)) < 0) {
-      fprintf(stderr, "ftdi_write_data failed: %d\n", ret);
+    // read into pins buffer to get MISO bit from previous falling clock
+    CHECK(spi_read_pins(spi));
+
+    // ensure clock is low
+    if ((spi->pins & 1<<BIT_SCK) != 0) {
+      fprintf(stderr, "spi_transfer expects low clock\n");
       return -1;
     }
+
+    // set MSB from data buffer into MOSI bit of pins buffer
+    spi->pins = (spi->pins & ~(1<<BIT_MO)) | ((data>>7) << BIT_MO);
+
+    // shift written MOSI bit off the right, set new MISO bit into the left.
+    data = (data << 1) | (spi->pins>>BIT_MI & 1);
+
+    // write MOSI bit before rising clock
+    CHECK(spi_write_pins(spi, spi->pins));
+
     // set SPI clock high; input data is latched on the rising edge
-    pins |= 1<<BIT_SCK;
-    print_pins(pins, "rising clock");
-    if ((ret = ftdi_write_data(ftdi, &pins, 1)) < 0) {
-      fprintf(stderr, "ftdi_write_data failed: %d\n", ret);
-      return -1;
-    }
-    // set SPI clock low; data is shifted out on the falling edge
-    pins &= ~(1<<BIT_SCK);
-    print_pins(pins, "falling clock");
-    if ((ret = ftdi_write_data(ftdi, &pins, 1)) < 0) {
-      fprintf(stderr, "ftdi_write_data failed: %d\n", ret);
-      return -1;
-    }
-    // read MISO bit from pins buffer into LSB of data buffer
-    if (ftdi_read_pins(ftdi, &pins) < 0) {
-      fprintf(stderr, "ftdi_read_pins failed\n");
-      return -1;
-    }
-    print_pins(pins, "read MISO");
-    // shift new MISO bit in from the left, discarding written MOSI bit off the left.
-    *data = (*data << 1) | (pins>>BIT_MI & 1);
-    //print_binary(*data);
+    CHECK(spi_write_pins(spi, spi->pins | 1<<BIT_SCK));
+
+    // set SPI clock low; data shifted out on falling edge will be read on next iteration
+    CHECK(spi_write_pins(spi, spi->pins & ~(1<<BIT_SCK)));
   }
+  if (debug) {
+    printf("MISO ← 0x%02X ", data);
+    print_binary(data);
+    printf("\n");
+  }
+  spi->data = data;
+  return 0;
+}
+
+int spi_read_pins(struct spi_context *spi) {
+  int ret;
+  if ((ret = ftdi_read_pins(spi->ftdi, &spi->pins)) < 0) {
+    fprintf(stderr, "spi_read_pins ftdi_read_pins failed: %d\n", ret);
+    return -1;
+  }
+  return 0;
+}
+
+int spi_write_pins(struct spi_context *spi, uint8_t pins) {
+  int ret;
+  if ((ret = ftdi_write_data(spi->ftdi, &pins, 1)) < 0) {
+    fprintf(stderr, "spi_write_pins ftdi_write_data failed: %d\n", ret);
+    return -1;
+  }
+  spi->pins = pins;
   return 0;
 }
 
@@ -164,5 +259,11 @@ void print_binary(uint8_t x) {
 }
 
 void print_pins(uint8_t p, char *msg) {
-  printf("CS:%d SCK:%d MOSI:%d MISO:%d (%s)\n", p>>BIT_CS&1, p>>BIT_SCK&1, p>>BIT_MO&1, p>>BIT_MI&1, msg);
+  printf("  CS:%d SCK:%d MOSI:%d MISO:%d (%s)\n",
+      p>>BIT_CS&1, p>>BIT_SCK&1, p>>BIT_MO&1, p>>BIT_MI&1, msg);
+}
+
+void print_status(uint8_t s) {
+  printf("status 0x%02X: WPEN:%d IPL:%d 0:%d LIP:%d BP:%d%d WEL:%d ~RDY:%d\n",
+      s, s>>7&1, s>>6&1, s>>5&1, s>>4&1, s>>3&1, s>>2&1, s>>1&1, s&1);
 }
