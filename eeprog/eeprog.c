94526a35b48e61011d8ef1934fa32b2bf8f7be8e
Paul Annesley <paul@annesley.cc>
2021-09-10 00:04:28 +1000

    eeprog write <filename> command; works.

--- eeprog/eeprog.c
+++ eeprog/eeprog.c
@@ -1,25 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 #include "spi.h"
 #include "hexdump.h"
 
 int test_things(struct spi_context *);
+int write(struct spi_context *, char *srcfile);
 
-int main(int argc, char *argv[])
-{
+/**
+ * Usage:
+ *     eeprog write <filename>
+ *     eeprog test
+ */
+int main(int argc, char *argv[]) {
   struct spi_context spi;
   if (spi_init(&spi) != 0) return EXIT_FAILURE;
 
-  if (test_things(&spi) != 0) {
-    fprintf(stderr, "test_things failed, hopefully it produced some stderr\n");
-    spi_deinit(&spi);
-    return EXIT_FAILURE;
+  if (argc == 3 && strcmp(argv[1], "write") == 0) {
+    printf("write\n");
+    if (write(&spi, argv[2]) != 0) {
+      fprintf(stderr, "test_things failed, hopefully it produced some stderr\n");
+      spi_deinit(&spi);
+      return EXIT_FAILURE;
+    }
+  } else if (argc == 2 && strcmp("test", argv[1]) == 0) {
+    if (test_things(&spi) != 0) {
+      fprintf(stderr, "test_things failed, hopefully it produced some stderr\n");
+      spi_deinit(&spi);
+      return EXIT_FAILURE;
+    }
   }
 
   if (spi_deinit(&spi) != 0) return EXIT_FAILURE;
 
   return EXIT_SUCCESS;
+}
+
+int write(struct spi_context *spi, char *srcfile) {
+  fprintf(stderr, "opening %s\n", srcfile);
+  FILE * f;
+  if ((f = fopen(srcfile, "r")) == NULL) {
+    perror("fopen");
+    return -1;
+  }
+  if (fseek(f, 0, SEEK_END) != 0) {
+    perror("fseek");
+    fclose(f);
+    return -1;
+  }
+  long size;
+  if ((size = ftell(f)) < 0) {
+    perror("ftell SEEK_END");
+    fclose(f);
+    return -1;
+  }
+  if (fseek(f, 0, SEEK_SET) != 0) {
+    perror("fseek SEEK_SET");
+    fclose(f);
+    return -1;
+  }
+  printf("file size: %ld\n", size);
+
+  printf("-- enable writes\n");
+  CHECK(spi_select(spi));
+  CHECK(spi_transfer(spi, EEPROM_WREN));
+  CHECK(spi_deselect(spi));
+
+  uint32_t addr;
+  addr = 0x000000;
+  printf("-- write data\n");
+  CHECK(spi_select(spi));
+  CHECK(spi_transfer(spi, EEPROM_WRITE));
+  CHECK(spi_transfer(spi, addr>>16&0xFF)); // ADDR[23:16]
+  CHECK(spi_transfer(spi, addr>>8&0xFF)); // ADDR[15:8]
+  CHECK(spi_transfer(spi, addr&0xFF)); // ADDR[7:0]
+  struct hexdump_context hd = {.output = stderr, .addr = addr};
+  uint8_t data;
+  for (int i = 0; i < size; i++) {
+    // TODO: fread() per byte is probably shit, but probably not the bottleneck.
+    if (fread(&data, 1, 1, f) < 1) {
+      fprintf(stderr, "problem reading file\n");
+    };
+    CHECK(spi_transfer(spi, data));
+    if (i % 256 == 255) {
+      addr += 256;
+      CHECK(spi_deselect(spi));
+
+      printf("-- enable writes\n");
+      CHECK(spi_select(spi));
+      CHECK(spi_transfer(spi, EEPROM_WREN));
+      CHECK(spi_deselect(spi));
+
+      printf("-- read status register\n");
+      CHECK(spi_select(spi));
+      CHECK(spi_transfer(spi, EEPROM_RDSR));
+      CHECK(spi_transfer(spi, 0));
+      print_status(spi->data);
+      CHECK(spi_deselect(spi));
+
+      CHECK(spi_select(spi));
+      CHECK(spi_transfer(spi, EEPROM_WRITE));
+      CHECK(spi_transfer(spi, addr>>16&0xFF)); // ADDR[23:16]
+      CHECK(spi_transfer(spi, addr>>8&0xFF)); // ADDR[15:8]
+      CHECK(spi_transfer(spi, addr&0xFF)); // ADDR[7:0]
+    }
+    hexdump_byte(&hd, data);
+  }
+  hexdump_finish(&hd);
+  CHECK(spi_deselect(spi));
+
+  for (int i = 0; i < size; i++) {
+  }
+  fclose(f);
+  return 0;
 }
 
 int test_things(struct spi_context *spi) {
@@ -37,7 +131,7 @@ int test_things(struct spi_context *spi) {
   print_status(spi->data);
   CHECK(spi_deselect(spi));
 
-  addr = 0x000380;
+  addr = 0x000000;
   printf("-- read data\n");
   CHECK(spi_select(spi));
   CHECK(spi_transfer(spi, EEPROM_READ));
@@ -45,7 +139,7 @@ int test_things(struct spi_context *spi) {
   CHECK(spi_transfer(spi, addr>>8&0xFF)); // ADDR[15:8]
   CHECK(spi_transfer(spi, addr&0xFF)); // ADDR[7:0]
   struct hexdump_context hd = {.output = stderr, .addr = addr};
-  for (int i = 0; i < 256; i++) {
+  for (int i = 0; i < 486; i++) {
     CHECK(spi_transfer(spi, 0));
     hexdump_byte(&hd, spi->data);
   }
