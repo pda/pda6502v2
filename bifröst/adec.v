5f31638bf8cb59dd2b3f4f74f242f020d786cee0
Paul Annesley <paul@annesley.cc>
2021-10-08 22:56:42 +1100

    bifröst adec: only ram_cs guarded by clock=HIGH
    GARTHWILSON reckon's RAM should be guarded by PHI2.
    Seems like clocked devices (6522 etc) don't need this.
    Hopefully that applies to SID/ARMSID too (it's clocked).
    http://forum.6502.org/viewtopic.php?t=511

--- bifröst/adec.v
+++ bifröst/adec.v
@@ -19,15 +19,15 @@ module adec(
 
   // SID: 5-bit; 32 registers.  The C64 repeats them 32 times from 0xD400
   // onwards, presumably to simplify address decoding. We'll do the same.
-  assign sid_cs     = ~(~clock && (addr >= 16'hD400 && addr <= 16'hD7FF)); // 5-bit; 32 registers
+  assign sid_cs     = ~(addr >= 16'hD400 && addr <= 16'hD7FF); // 5-bit; 32 registers
 
   // C64 0xD800–DBFF: color RAM
 
   // C64 0xDC00–DCFF: CIA#1; inputs (keyboard, joystick, mouse), datasette, IRQ control
   // C64 0xDD00–DDFF: CIA#2; serial bus, RS232, NMI control
-  assign via1_cs    = ~(~clock && (addr >= 16'hDC00 && addr <= 16'hDC0F)); // 4-bit; 16 registers
-  assign via2_cs    = ~(~clock && (addr >= 16'hDC10 && addr <= 16'hDC1F)); // 4-bit; 16 registers
-  assign uart_cs    = ~(~clock && (addr >= 16'hDC20 && addr <= 16'hDC2F)); // 4-bit; 16 registers
+  assign via1_cs    = ~(addr >= 16'hDC00 && addr <= 16'hDC0F); // 4-bit; 16 registers
+  assign via2_cs    = ~(addr >= 16'hDC10 && addr <= 16'hDC1F); // 4-bit; 16 registers
+  assign uart_cs    = ~(addr >= 16'hDC20 && addr <= 16'hDC2F); // 4-bit; 16 registers
 
   // C64 0xDE00–DEFF: I/O Area #1 (external/optional)
   // C64 0xDF00–DFFF: I/O Area #2 (external/optional)
