ed8f028f637b6097ea394e591b6cda186676d177
Paul Annesley <paul@annesley.cc>
2021-10-26 23:32:21 +1100

    os: UART echo via interrupts, not polling

--- os/os.s
+++ os/os.s
@@ -1,40 +1,53 @@
-.import BlinkenStart
-.import BlinkenTick
-.import UartMain
+.import BlinkenStart, BlinkenTick
+.import UartMain, UartRxInterrupt
 
 .import BLINKEN, BLINKSRC
 .import VIA1, VIA2, VIA_IFR : zp
+.import UART, UART_ISR : zp;
 
 .segment "os"
 
 .PROC Main
-          JSR BlinkenStart
+          ;JSR BlinkenStart
+          LDA #$04           ; 0:reg 1:data 2:addrl 3:addrh 4:IRQ
+          STA BLINKSRC
           JSR UartMain
-          ;LDA #$04           ; 0:reg 1:data 2:addrl 3:addrh 4:IRQ
-          ;STA BLINKSRC
 halt:     JMP halt
 .ENDPROC
 
 
-HandleReset:
+.PROC HandleReset
           SEI                 ; mask interrupts during start-up
           LDX #$FF            ;
           TXS                 ; set stack pointer to $ff ($01FF)
           CLI                 ; resume interrupts
           CLD                 ; don't be in crazy decimal mode.
           JMP Main
+.ENDPROC
 
-HandleInterrupt:
+.PROC HandleInterrupt
+          PHA
           BIT VIA1+VIA_IFR
           BPL after_via1      ; not VIA1 interrupt
           BVC after_via1_t1   ; not VIA1 T1 interrupt
-          JSR BlinkenTick
+          JSR BlinkenTick     ; VIA1 T1 animates BLINKEN
 after_via1_t1:
 after_via1:
+          BIT VIA2+VIA_IFR
+          BPL after_via2      ; not VIA2 interrupt
+after_via2:
+          LDA #%00000010
+          AND UART+UART_ISR
+          BEQ after_uart      ; not UART RX interrupt
+          JSR UartRxInterrupt
+after_uart:
+          PLA
           RTI
+.ENDPROC
 
-HandleNonMaskableInterrupt:
+.PROC HandleNonMaskableInterrupt
           RTI
+.ENDPROC
 
 .segment "vectors"
 
--- os/uart.s
+++ os/uart.s
@@ -1,45 +1,47 @@
 .segment "os"
 
 .export UartMain
+.export UartRxInterrupt
 
-UART_MRA      = $DC20 ; read + write
-UART_SRA      = $DC21 ; read
-UART_CSRA     = $DC21 ; write
-UART_CRA      = $DC22 ; write
-UART_RXFIFOA  = $DC23 ; read
-UART_TXFIFOA  = $DC23 ; write
-UART_IPCR     = $DC24 ; read
-UART_ACR      = $DC24 ; write
-UART_ISR      = $DC25 ; read
-UART_IMR      = $DC25 ; write
-UART_CTU      = $DC26 ; read
-UART_CTPU     = $DC26 ; write
-UART_CTL      = $DC27 ; read
-UART_CTPL     = $DC27 ; write
-UART_MRB      = $DC28 ; read + write
-UART_SRB      = $DC29 ; read
-UART_CSRB     = $DC29 ; write
-UART_CRB      = $DC2A ; write
-UART_RXFIFOB  = $DC2B ; read
-UART_TXFIFOB  = $DC2B ; write
-UART_MISC     = $DC2C ; read + write
-UART_IPR      = $DC2D ; read
-UART_OPCR     = $DC2D ; write
-UART_SOPR     = $DC2E ; write
-UART_ROPR     = $DC2F ; write
+.export UART := $DC20
+
+.export UART_MRA      = $0 ; read + write
+.export UART_SRA      = $1 ; read
+.export UART_CSRA     = $1 ; write
+.export UART_CRA      = $2 ; write
+.export UART_RXFIFOA  = $3 ; read
+.export UART_TXFIFOA  = $3 ; write
+.export UART_IPCR     = $4 ; read
+.export UART_ACR      = $4 ; write
+.export UART_ISR      = $5 ; read
+.export UART_IMR      = $5 ; write
+.export UART_CTU      = $6 ; read
+.export UART_CTPU     = $6 ; write
+.export UART_CTL      = $7 ; read
+.export UART_CTPL     = $7 ; write
+.export UART_MRB      = $8 ; read + write
+.export UART_SRB      = $9 ; read
+.export UART_CSRB     = $9 ; write
+.export UART_CRB      = $A ; write
+.export UART_RXFIFOB  = $B ; read
+.export UART_TXFIFOB  = $B ; write
+.export UART_MISC     = $C ; read + write
+.export UART_IPR      = $D ; read
+.export UART_OPCR     = $D ; write
+.export UART_SOPR     = $E ; write
+.export UART_ROPR     = $F ; write
 
 .PROC UartMain
           JSR UartConfigure
           JSR UartHello
-forever:  JSR UartEcho
-          JMP forever
+          RTS
 .ENDPROC
 
 .PROC UartConfigure
           ; Mode Select Registers
           LDA #%10110000      ; Select MR0A
-          STA UART_CRA
-          LDA #%00001000      ; Mode Register 0 channel A (MR0A)
+          STA UART+UART_CRA
+          LDA #%10001000      ; Mode Register 0 channel A (MR0A)
           ;     |||||||+-------->   0: baud rate extended I
           ;     ||||||+--------->   1: TEST2
           ;     |||||+---------->   2: baud rate extended II
@@ -47,51 +49,66 @@ forever:  JSR UartEcho
           ;     ||++------------> 5:4: TX Interrupt fill level
           ;     |+-------------->   6: RxINT[2] fill level
           ;     +--------------->   7: RxWATCHDOG
-          STA UART_MRA
+          STA UART+UART_MRA
           LDA #%00010000      ; Reset to MR1A
-          STA UART_CRA
-          LDA #%10010011      ; Mode Register 1 channel A (MR1A)
+          STA UART+UART_CRA
+          LDA #%11010011      ; Mode Register 1 channel A (MR1A)
           ;     ||||||++--------> 1:0: bits per char (5,6,7,*8*)
           ;     |||||+---------->   2: parity type (even, odd)
           ;     |||++-----------> 4:3: parity mode (with, force, *no*, multi)
           ;     ||+------------->   5: error mode (char, block)
           ;     |+-------------->   6: RxINT[1]
           ;     +--------------->   7: RxRTS (also needs OPR[0]=1)
-          STA UART_MRA        ; auto advances to MR2A after this
+          STA UART+UART_MRA        ; auto advances to MR2A after this
           LDA #%00110111      ; MR2A
           ;     ||||++++--------> 3:0 stop bit length (111=0x7: 1.000)
           ;     |||+------------>   4 CTSN Enable Tx (1: CTS on)
           ;     ||+------------->   5 RTSN Control Tx (1: RTS on)
           ;     ++--------------> 6:7 Channel mode (normal, echo, lloop, rloop)
-          STA UART_MRA        ; Mode Register 2 channel A (MR2A)
+          STA UART+UART_MRA        ; Mode Register 2 channel A (MR2A)
           LDA #%10111011      ; Clock Select Register
           ;     ||||++++--------> 3:0: TX baud (1011 w/ MR0[0]=0: 9,600)
           ;     ++++------------> 7:4: RX baud
-          STA UART_CSRA
+          STA UART+UART_CSRA
           LDA #%01100000      ; Auxiliary Control Register
           ;     ||||++++--------> 3:0: IP3:0 change interrupt enable
           ;     |+++------------> 6:4: counter/timer mode/clock source (110=X1/CLK)
           ;     +--------------->   7: baud rate generator select
-          STA UART_ACR
+          STA UART+UART_ACR
           LDA #%00000001      ; Set Output Port bits Register (SOPR)
           ;            +-------->   0: needed for MR1A[7] RxRTS=1
-          STA UART_SOPR
+          STA UART+UART_SOPR
+          LDA #%00000010      ; Interrupt mask register (IMR)
+          ;     |||||||+-------->   0: TxRDYA
+          ;     ||||||+--------->   1: RxRDYA / FFULLA
+          ;     |||||+---------->   2: channel A change in break
+          ;     ||||+----------->   3: counter ready
+          ;     |||+------------>   4: TxRDYB
+          ;     ||+------------->   5: RxRDYB
+          ;     |+-------------->   6: channel B change in break
+          ;     +--------------->   7: IP0[3:0] change (subject to ACR[3:0])
+          STA UART+UART_IMR
           LDA #%00000101      ; Command Register A (CRA)
           ;          | +-------->   0: enable RX
           ;          +---------->   2: enable TX
-          STA UART_CRA
+          STA UART+UART_CRA
+          RTS
+.ENDPROC
+
+.PROC UartRxInterrupt
+          JSR UartEcho        ; tmp/demo: easy way to flush rx fifo?
           RTS
 .ENDPROC
 
 .PROC UartHello
           LDX #0
-msgloop:  LDA UART_SRA
+msgloop:  LDA UART+UART_SRA
           AND #1<<2           ; TxRDYA
           BEQ msgloop         ; keep waiting
 
           LDA message,X
           BEQ msgdone
-          STA UART_TXFIFOA
+          STA UART+UART_TXFIFOA
 
           INX
           JMP msgloop
@@ -99,10 +116,13 @@ msgdone:  RTS
 .ENDPROC
 
 .PROC UartEcho
+          PHA
+          PHX
+again:
           LDA #1<<0           ; RxRDY: char is waiting in RX FIFO
-          BIT UART_SRA
+          BIT UART+UART_SRA
           BEQ empty
-          LDX UART_RXFIFOA
+          LDX UART+UART_RXFIFOA
           JSR UartPutc        ; TX the RX char
           TXA
           CMP #$0D            ; if RX was CR
@@ -116,16 +136,18 @@ notcr:    TXA
           JSR UartPutc
           LDX #$08            ; backspace
           JSR UartPutc
-notbs:    JMP UartEcho        ; check for more
-empty:    RTS
+notbs:    JMP again        ; check for more
+empty:    PLX
+          PLA
+          RTS
 .ENDPROC
 
 ; X: char to put on UART FIFO
 .PROC UartPutc
           LDA #1<<2           ; TxRDY: TX FIFO is not full
-waitloop: BIT UART_SRA
+waitloop: BIT UART+UART_SRA
           BEQ waitloop
-          STX UART_TXFIFOA
+          STX UART+UART_TXFIFOA
           RTS
 .ENDPROC
 
