a4ef096fac6f0b405536ab0fa2c359f9c8fac988
Paul Annesley <paul@annesley.cc>
2021-11-13 20:25:16 +1100

    os: game of life refactored to use actual maths and comments.
    Previous code was a 2am special.

--- os/life.s
+++ os/life.s
@@ -101,127 +101,75 @@ return:         RTS
 ; A is used for rando stuff
 ; bit 7 of each byte indicates liveness
 .proc LifeTick
-                LDX #0
+                LDX #0                  ; cell index
 eachcell:       LDY #0                  ; neighbor count
-                ; TODO: obvs not this shit to get to offset -17
-                DEX ; - 1
-                DEX ; - 2
-                DEX ; - 3
-                DEX ; - 4
-                DEX ; - 5
-                DEX ; - 6
-                DEX ; - 7
-                DEX ; - 8
-                DEX ; - 9
-                DEX ; -10
-                DEX ; -11
-                DEX ; -12
-                DEX ; -13
-                DEX ; -14
-                DEX ; -15
-                DEX ; -16
-                DEX ; -17
+                TXA                     ; transfer cell index to A for subtraction
+                SEC                     ; prepare carry bit for subtraction
+                SBC #17                 ; jump back a full row (16) plus one cell (origin->NW neighbor)
+                TAX                     ; transfer result back into X for indexed addressing
                 BIT GRIDADDR,X          ; NW alive?
-                BPL nopeNW
-                INY                     ; increment neighbor count
-nopeNW:         INX ; -16
+                BPL deadNW
+                INY                     ; add NW neighbor to tally
+deadNW:         INX                     ; move from NW to N neighbor (origin-16)
                 BIT GRIDADDR,X          ; N alive?
-                BPL nopeN
-                INY                     ; increment neighbor count
-nopeN:          INX ; -15
+                BPL deadN
+                INY                     ; add N neighbor to tally
+deadN:          INX                     ; move from N->NE neighbor (origin-15)
                 BIT GRIDADDR,X          ; NE alive?
-                BPL nopeNE
-                INY                     ; increment neighbor count
-nopeNE:         ; TODO: not this shit
-                INX ; -14
-                INX ; -13
-                INX ; -12
-                INX ; -11
-                INX ; -10
-                INX ; - 9
-                INX ; - 8
-                INX ; - 7
-                INX ; - 6
-                INX ; - 5
-                INX ; - 4
-                INX ; - 3
-                INX ; - 2
-                INX ; - 1
+                BPL deadNE
+                INY                     ; add N neighbor to tally
+deadNE:         TXA                     ; transfer cell index to A for addition
+                CLC                     ; prepare carry bit for addition
+                ADC #14                 ; jump one row (16) minus two cells (NE->W) (origin-1)
+                TAX                     ; transfer result back into X for indexed addressing
                 BIT GRIDADDR,X          ; W alive?
-                BPL nopeW
-                INY                     ; increment neighbor count
-nopeW:          INX ;   0
-                INX ; + 1
+                BPL deadW
+                INY                     ; add W neighbor to tally
+deadW:          INX                     ; move W->origin, skip this one
+                INX                     ; move origin->E (origin+1)
                 BIT GRIDADDR,X          ; E alive?
-                BPL nopeE
-                INY                     ; increment neighbor count
-nopeE:          INX ; + 2
-                INX ; + 3
-                INX ; + 4
-                INX ; + 5
-                INX ; + 6
-                INX ; + 7
-                INX ; + 8
-                INX ; + 9
-                INX ; +10
-                INX ; +11
-                INX ; +12
-                INX ; +13
-                INX ; +14
-                INX ; +15
+                BPL deadE
+                INY                     ; add E neighbor to tally
+deadE:          TXA                     ; transfer cell index to A for addition
+                CLC                     ; prepare carry bit for addition
+                ADC #14                 ; jump one row (16) minus two cells (E->SW) (origin+15)
+                TAX                     ; transfer result back into X for indexed addressing
                 BIT GRIDADDR,X          ; SW alive?
-                BPL nopeSW
-                INY                     ; increment neighbor count
-nopeSW:         INX ; +16
+                BPL deadSW
+                INY                     ; add SW neighbor to tally
+deadSW:         INX                     ; move SW->S (origin+16)
                 BIT GRIDADDR,X          ; S alive?
-                BPL nopeS
-                INY                     ; increment neighbor count
-nopeS:          INX ; +17
+                BPL deadS
+                INY                     ; add S neighbor to tally
+deadS:          INX                     ; move S->SE (origin+17)
                 BIT GRIDADDR,X          ; SE alive?
-                BPL nopeSE
-                INY                     ; increment neighbor count
-nopeSE:         ; TODO: not _any_ of this shit (get back to original cell)
-                DEX ; -16
-                DEX ; -15
-                DEX ; -14
-                DEX ; -13
-                DEX ; -12
-                DEX ; -11
-                DEX ; -10
-                DEX ; - 9
-                DEX ; - 8
-                DEX ; - 7
-                DEX ; - 6
-                DEX ; - 5
-                DEX ; - 4
-                DEX ; - 3
-                DEX ; - 2
-                DEX ; - 1
-                DEX ;   0
-                ; after that horror-show, Y should be the alive-neighbor count
-                TYA
-                BIT GRIDADDR,X          ; cell alive?
-                BPL wasdead
-wasalive:       CMP #2                  ; if this is negative, N <= 1
-                BMI makedead
-                CMP #4                  ; if this is negative, N <= 3 (and N <= 1)
-                BMI makealive
-                JMP makedead            ; otherwise, N > 3
-wasdead:        CMP #3
-                BEQ makealive
-                JMP makedead
-makealive:      LDA #$FF
+                BPL deadSE
+                INY                     ; add SE neighbor to tally
+deadSE:         TXA                     ; transfer cell index to A for subtraction
+                SEC                     ; prepare carry bit for subtraction
+                SBC #17                 ; jump one row (16) plus one cell (SE->origin)
+                TAX                     ; transfer result back into X for indexed addressing
+                TYA                     ; transfer alive-neighbor tally to A
+                BIT GRIDADDR,X          ; origin cell alive?
+                BPL origindead
+originalive:    CMP #2                  ; if neighbors<=1 (i.e. neighbors-2 is negative)
+                BMI makedead            ; our living cell dies from underpopulation
+                CMP #4                  ; if neighbors<=3 (i.e. neighbors-4 is is negative)
+                BMI makealive           ; our living cell lives on, 2..3 neighbors
+                JMP makedead            ; else neighbors>3, our living cell dies from overpopulation
+origindead:     CMP #3                  ; if neighbors==3
+                BEQ makealive           ; our empty cell spawns life
+                JMP makedead            ; else it stays dead
+makealive:      LDA #$FF                ; prepare to set our cell's bit 7 (and other bits for good measure)
                 JMP writecell
-makedead:       LDA #$00
-writecell:      STA GRIDADDRB,X         ; write the new state into the shadow grid
-donecell:       INX
-                BEQ doneallcells        ; branch else JMP because target is too far away ;(
-                JMP eachcell
-doneallcells:
-copyeachcell:   LDA GRIDADDRB,X
-                STA GRIDADDR,X
+makedead:       LDA #$00                ; prepare to clear our cell's bit 7 (and other bits, for good measure)
+writecell:      STA GRIDADDRB,X         ; write the new state into the "next generation" grid.
+donecell:       INX                     ; Move to the next cell.
+                BNE eachcell            ; process it unless we've wrapped around to zero.
+copyeachcell:   LDA GRIDADDRB,X         ; All cells evaluated; copy each cell from the next generation grid...
+                STA GRIDADDR,X          ; ... into the primary grid.
                 INX
-                BNE copyeachcell
+                BNE copyeachcell        ; Until we wrap around to zero.
                 RTS
 .endproc
 
