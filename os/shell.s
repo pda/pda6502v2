e3ed0d4eb51bc264858aeb5290ad264218560045
Paul Annesley <paul@annesley.cc>
2021-11-15 20:45:28 +1100

    os: StrEq uses R0, R2 introduced in a7d980b instead of $00 & $02

--- os/shell.s
+++ os/shell.s
@@ -132,12 +132,12 @@ return:         RTS
 
 ; StrEq compares two null-terminated strings.
 ; The results is returned by the Z bit of the status register.
-; Args are ZP pointers $00 and $02
+; Args are ZP register pointers R0,R1 and R2,R3
 ; Maximum string length is 255 plus null-terminator.
 .proc StrEq
                 LDY #0
-loop:           LDA ($00),Y             ; load byte from string A
-                CMP ($02),Y             ; load byte from string B
+loop:           LDA (R0),Y             ; load byte from string A
+                CMP (R2),Y             ; load byte from string B
                 BNE return              ; if this byte differs, strings aren't equal (Z=1 ready for return)
                 CMP #0                  ; the bytes are equal; was it null?
                 BEQ return              ; in which case the strings match (Z=0 ready for return)
