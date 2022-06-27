73c3c3d71efa6222290feda09cf081c3dd84e5c9
Paul Annesley <paul@annesley.cc>
2022-06-27 20:58:24 +0930

    emu: main tidied & commented

--- emu/src/main.rs
+++ emu/src/main.rs
@@ -3,35 +3,15 @@ mod bus;
 mod cpu;
 mod isa;
 
-use crate::asm::Assembler;
-use crate::bus::Bus;
-use crate::cpu::Cpu;
-
 fn main() {
-    let asm = asm();
-    println!("\n{}", asm);
-
-    let mut bus = Bus::default();
-    for (i, byte) in asm.assemble().unwrap().iter().enumerate() {
-        bus.write(asm.org + (i as u16), *byte);
-    }
-
-    // set reset vector to base address where program is installed
-    bus.write(0xFFFC, asm.org as u8);
-    bus.write(0xFFFD, (asm.org >> 8) as u8);
-
-    let mut cpu = Cpu::new(bus);
-    cpu.reset();
-
-    for _ in 0..10 {
-        println!("{:?}", cpu);
-        cpu.step();
-    }
-}
-
-fn asm() -> Assembler {
+    // syntax brevity for Assembler args
     use asm::{label, val, Operand::*};
-    let mut asm = Assembler::new();
+
+    // prepare an address bus
+    let mut bus = bus::Bus::default();
+
+    // assemble a trivial demo program
+    let mut asm = asm::Assembler::new();
     asm.org(0x1234)
         .nop()
         .ldx(Imm(0x10))
@@ -40,5 +20,25 @@ fn asm() -> Assembler {
         .nop()
         .jmp(Abs(label("loop")))
         .jmp(Abs(val(0)));
-    asm
+
+    // print assembly listing
+    println!("\n{}", asm);
+
+    // preload program to RAM
+    for (i, byte) in asm.assemble().unwrap().iter().enumerate() {
+        bus.write(asm.org + (i as u16), *byte);
+    }
+
+    // set reset vector to program address
+    bus.write(0xFFFC, asm.org as u8);
+    bus.write(0xFFFD, (asm.org >> 8) as u8);
+
+    let mut cpu = cpu::Cpu::new(bus);
+    cpu.reset();
+
+    // run some instructions
+    for _ in 0..10 {
+        println!("{:?}", cpu);
+        cpu.step();
+    }
 }
