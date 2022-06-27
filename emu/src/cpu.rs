ddebee121d8230215079a3c95f6ef99fca795cd0
Paul Annesley <paul@annesley.cc>
2022-06-27 21:17:57 +0930

    emu: cpu tidied & commented

--- emu/src/cpu.rs
+++ emu/src/cpu.rs
@@ -1,25 +1,23 @@
-use crate::bus::Bus;
-use crate::isa::instruction_list;
-use crate::isa::AddressMode;
-use crate::isa::Instruction;
-use crate::isa::Mnemonic;
+use std::fmt;
 
-use core::fmt::Debug;
-use core::fmt::Formatter;
+use crate::bus;
+use crate::isa;
 
+// A 65C02-like CPU
 pub struct Cpu {
-    bus: Bus,
-    pc: u16,
-    sp: u8,
-    a: u8,
-    x: u8,
-    y: u8,
-    sr: u8,
-    optab: [Option<Instruction>; 256],
+    bus: bus::Bus,
+    pc: u16, // program counter
+    sp: u8,  // stack pointer
+    a: u8,   // accumulator
+    x: u8,   // X register
+    y: u8,   // Y register
+    sr: u8,  // status register
+
+    optab: [Option<isa::Instruction>; 256],
 }
 
 impl Cpu {
-    pub fn new(bus: Bus) -> Cpu {
+    pub fn new(bus: bus::Bus) -> Cpu {
         Cpu {
             bus,
             pc: 0,
@@ -32,8 +30,9 @@ impl Cpu {
         }
     }
 
+    // Reset internal CPU state, as if the reset line had been asserted.
     pub fn reset(&mut self) {
-        self.pc = self.bus.read16(0xFFFC); // TODO: load vector
+        self.pc = self.bus.read16(0xFFFC);
         self.sp = 0x00;
         self.a = 0x00;
         self.x = 0x00;
@@ -41,6 +40,7 @@ impl Cpu {
         self.sr = 0x00;
     }
 
+    // Load and execute a single instruction.
     pub fn step(&mut self) {
         let opcode = self.bus.read(self.pc);
         self.pc = self.pc.wrapping_add(1);
@@ -50,51 +50,54 @@ impl Cpu {
         }
     }
 
-    fn execute(&mut self, instruction: Instruction) {
-        use AddressMode::*;
+    // Execute an instruction, reading the operands for its address mode from the bus.
+    fn execute(&mut self, instruction: isa::Instruction) {
+        use isa::AddressMode::*;
+        use isa::Mnemonic as M;
+
         println!("{:?}", instruction);
         match instruction.mnemonic {
-            // Mnemonic::ADC => {}
-            // Mnemonic::AND => {}
-            // Mnemonic::ASL => {}
-            // Mnemonic::BCC => {}
-            // Mnemonic::BCS => {}
-            // Mnemonic::BEQ => {}
-            // Mnemonic::BIT => {}
-            // Mnemonic::BMI => {}
-            // Mnemonic::BNE => {}
-            // Mnemonic::BPL => {}
-            // Mnemonic::BRK => {}
-            // Mnemonic::BVC => {}
-            // Mnemonic::BVS => {}
-            // Mnemonic::CLC => {}
-            // Mnemonic::CLD => {}
-            // Mnemonic::CLI => {}
-            // Mnemonic::CLV => {}
-            // Mnemonic::CMP => {}
-            // Mnemonic::CPX => {}
-            // Mnemonic::CPY => {}
-            // Mnemonic::DEC => {}
-            // Mnemonic::DEX => {}
-            // Mnemonic::DEY => {}
-            // Mnemonic::EOR => {}
-            // Mnemonic::INC => {}
-            Mnemonic::INX => match instruction.mode {
+            // M::ADC => {}
+            // M::AND => {}
+            // M::ASL => {}
+            // M::BCC => {}
+            // M::BCS => {}
+            // M::BEQ => {}
+            // M::BIT => {}
+            // M::BMI => {}
+            // M::BNE => {}
+            // M::BPL => {}
+            // M::BRK => {}
+            // M::BVC => {}
+            // M::BVS => {}
+            // M::CLC => {}
+            // M::CLD => {}
+            // M::CLI => {}
+            // M::CLV => {}
+            // M::CMP => {}
+            // M::CPX => {}
+            // M::CPY => {}
+            // M::DEC => {}
+            // M::DEX => {}
+            // M::DEY => {}
+            // M::EOR => {}
+            // M::INC => {}
+            M::INX => match instruction.mode {
                 Implied => {
                     self.x = self.x.wrapping_add(1);
                     // TODO: update self.status N and Z bits
                 }
                 other => panic!("illegal AddressMode: {:?}", other),
             },
-            // Mnemonic::INY => {}
-            Mnemonic::JMP => match instruction.mode {
+            // M::INY => {}
+            M::JMP => match instruction.mode {
                 Absolute => self.pc = self.bus.read16(self.pc),
                 Indirect => todo!("{:?}", instruction.mode),
                 other => panic!("illegal AddressMode: {:?}", other),
             },
-            // Mnemonic::JSR => {}
-            // Mnemonic::LDA => {}
-            Mnemonic::LDX => match instruction.mode {
+            // M::JSR => {}
+            // M::LDA => {}
+            M::LDX => match instruction.mode {
                 Immediate => {
                     self.x = self.bus.read(self.pc);
                     self.pc += 1;
@@ -103,49 +106,39 @@ impl Cpu {
                 Zeropage | ZeropageY | Absolute | AbsoluteY => todo!("{:?}", instruction.mode),
                 other => panic!("illegal AddressMode: {:?}", other),
             },
-            // Mnemonic::LDY => {}
-            // Mnemonic::LSR => {}
-            Mnemonic::NOP => {}
-            // Mnemonic::ORA => {}
-            // Mnemonic::PHA => {}
-            // Mnemonic::PHP => {}
-            // Mnemonic::PLA => {}
-            // Mnemonic::PLP => {}
-            // Mnemonic::ROL => {}
-            // Mnemonic::ROR => {}
-            // Mnemonic::RTI => {}
-            // Mnemonic::RTS => {}
-            // Mnemonic::SBC => {}
-            // Mnemonic::SEC => {}
-            // Mnemonic::SED => {}
-            // Mnemonic::SEI => {}
-            // Mnemonic::STA => {}
-            // Mnemonic::STX => {}
-            // Mnemonic::STY => {}
-            // Mnemonic::TAX => {}
-            // Mnemonic::TAY => {}
-            // Mnemonic::TSX => {}
-            // Mnemonic::TXA => {}
-            // Mnemonic::TXS => {}
-            // Mnemonic::TYA => {}
+            // M::LDY => {}
+            // M::LSR => {}
+            M::NOP => {}
+            // M::ORA => {}
+            // M::PHA => {}
+            // M::PHP => {}
+            // M::PLA => {}
+            // M::PLP => {}
+            // M::ROL => {}
+            // M::ROR => {}
+            // M::RTI => {}
+            // M::RTS => {}
+            // M::SBC => {}
+            // M::SEC => {}
+            // M::SED => {}
+            // M::SEI => {}
+            // M::STA => {}
+            // M::STX => {}
+            // M::STY => {}
+            // M::TAX => {}
+            // M::TAY => {}
+            // M::TSX => {}
+            // M::TXA => {}
+            // M::TXS => {}
+            // M::TYA => {}
             other => todo!("{:?}", other),
         }
     }
 }
 
-impl Debug for Cpu {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let stat: String = "nv-bdizc"
-            .chars()
-            .enumerate()
-            .map(|(i, x)| {
-                if self.sr >> (7 - i) & 1 == 1 {
-                    x.to_ascii_uppercase()
-                } else {
-                    x
-                }
-            })
-            .collect();
+impl fmt::Debug for Cpu {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+        let stat = stat(&self.sr);
         f.write_fmt(format_args!(
             "Cpu {{ SR: {} PC: ${:04X} SP: ${:02X} A: ${:02X} X: ${:02X} Y: ${:02X} }}",
             stat, self.pc, self.sp, self.a, self.x, self.y,
@@ -153,11 +146,27 @@ impl Debug for Cpu {
     }
 }
 
-pub fn build_opcode_table() -> [Option<Instruction>; 256] {
+// a string representation of the 8-bit status register;
+// enabled bits are upper case, disabled bits are lower case.
+fn stat(sr: &u8) -> String {
+    "nv-bdizc"
+        .chars()
+        .enumerate()
+        .map(|(i, x)| {
+            if sr >> (7 - i) & 1 == 1 {
+                x.to_ascii_uppercase()
+            } else {
+                x
+            }
+        })
+        .collect()
+}
+
+// Build an array of all known ISA instructions indexed by their opcode.
+pub fn build_opcode_table() -> [Option<isa::Instruction>; 256] {
     let mut optab = [None; 256];
-    for opcode in instruction_list() {
-        let code = opcode.code as usize;
-        optab[code] = Some(opcode);
+    for opcode in isa::instruction_list() {
+        optab[opcode.code as usize] = Some(opcode);
     }
-    return optab;
+    optab
 }
